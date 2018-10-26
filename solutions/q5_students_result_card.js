89746a57d1f0eac94e7ce75e60ac4c75ddc1e9bc
Ubuntu <ubuntu@ip-172-31-39-87.ec2.internal>
2018-10-26 12:13:10 +0000

    Q5 done.

--- solutions/q5_students_result_card.js
+++ solutions/q5_students_result_card.js
@@ -8,3 +8,55 @@
 
 
 // Write your code here
+
+class Student {
+    constructor(name, grammarMarks, accMarks, physicsMarks) {
+        this.name = name;
+        this.grammarMarks = grammarMarks;
+        this.accMarks = accMarks;
+        this.physicsMarks = physicsMarks;
+    }
+
+    showResultCard() {
+        var studentList = [];
+        var grammarMarksTot = 100;
+        var accMarksTot = 100;
+        var physicsMarksTot = 100;
+
+        for ( let i = 1; i <= 50; i++ ) {
+            var student = new Student;
+            student.name = 'Name'+ i;
+            student.grammarMarks = (Math.floor(Math.random() * 100));
+            student.accMarks = (Math.floor(Math.random() * 100))
+            studentList[i] = student;
+        }
+
+        for ( let i = 51; i <= 100; i++ ) {
+            var student = new Student;
+            student.name = 'Name'+ i;
+            student.grammarMarks = (Math.floor(Math.random() * 100));
+            student.physicsMarks = (Math.floor(Math.random() * 100))
+            studentList[i] = student;
+        }
+        
+        console.log("========================================");
+        console.log("RESULT CARD");
+        console.log("========================================");
+        var result=new String("Name         Percentage\n");
+        result = result + "----------------------------------------\n";
+        for ( let i = 1; i <= 50; i++ ) {
+            var percent = (((studentList[i].grammarMarks + studentList[i].accMarks)/(grammarMarksTot + accMarksTot)) * 100).toFixed(2);
+            result = result + studentList[i].name + "         " + percent + "\n";
+        }
+
+        for ( let i = 51; i <= 100; i++ ) {
+            var percent = (((studentList[i].grammarMarks + studentList[i].physicsMarks)/(grammarMarksTot + physicsMarksTot)) * 100).toFixed(2);
+            result = result + studentList[i].name + "         " + percent + "\n";
+        }
+
+        console.log(result);
+        }
+}
+
+let printer = new Student;
+printer.showResultCard();
