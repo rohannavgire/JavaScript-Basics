f8d047dfe61ab09df844de18176a694563985157
Ubuntu <ubuntu@ip-172-31-39-87.ec2.internal>
2018-10-26 07:19:04 +0000

    Q2 in progress

--- solutions/q2_array_to_object_converter.js
+++ solutions/q2_array_to_object_converter.js
@@ -2,13 +2,30 @@
 	based on a given key */
 
 
-const convert = (objArray, key) =>	
+const convert = (objArray, key) => {
+	if (Array.isArray(objArray))	{
 		objArray.reduce((obj, item) => {
-			obj[item[key]]=item;			
+			obj[item[key]]=item;
+			console.log(typeof(obj));					
 			return obj;
-		},
-		{}
-		)		
+		}, {});
+	}
+	else
+		return null;
+};	
+		
+/*
+const flatten = (arr) => {
+    if (Array.isArray(arr))	{
+        return arr.reduce(function (flat, toFlatten) {
+            return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+        }, []); 
+    }
+    else
+        return null;  
+};
+*/
+
 
 /* For example,
 INPUT - convert([{id: 1, value: 'abc'}, {id: 2, value: 'xyz'}], 'id')
@@ -20,7 +37,7 @@ OUTPUT - {
 
 */
 
-const object=convert([{id: 1, value: 'abc'}, {id: 2, value: 'xyz'}, {id: 3, value: 'pqr'}], 'id');
-console.log(object);
+//const object=convert([{id: 1, value: 'abc'}, {id: 2, value: 'xyz'}, {id: 3, value: 'pqr'}], 'id');
+//console.log(object);
 
 module.exports = convert;
